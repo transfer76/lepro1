40296de31c4cf351e248009623d63f47a2488a20
transfer76 <shina76@ukr.net>
2018-11-04 15:31:06 +0200

    make @db

--- app.rb
+++ app.rb
@@ -1,16 +1,26 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3' 
+
+def init_db
+	@db = SQlite3::Database.new 'lepro1.db'
+	@db.results_as_hash = true
+end
+
+before do
+	init_db
+end	
 
 get '/' do
 	erb "Hello World"
 end
 
 get '/new' do
- erb :new
+ 	erb :new
 end
 
 post '/new' do
-  content=params[:content]
-  erb "You typed #{content}"
+  	content=params[:content]
+  	erb "You typed #{content}"
 end
